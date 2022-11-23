ea3615e2a676e993f7ff39fe42186b1a776cf428
Machado <joaopedropiguinho@gmail.com>
2022-11-23 15:30:11 -0300

    att
    1.0

---
+++ machadopi.php
@@ -0,0 +1,17 @@
+<?php
+// include de header
+include_once './includes/_header.php';
+// parte do conteudo da pagina
+?>
+
+<main class="container-fluid">
+    <h1 id="h1machado">A EVOLUÇÃO DAS PISTAS</h1>
+    <div class="row">   
+        <img src="assets\IMGs\formula4.jpg" alt="" srcset="" id="imgformula">
+        <p id="textop1">Ultimamente o grid vem tendo os seus favoritos, corredores como Max Verstappen ou Charles Leclercq. É uma pena todo esse favoritismo imposto em cima dos mesmos corredores. A nova geração da fórmula 1 deixa a desejar com seus carros praticamente automáticos, os melhores pilotos são apenas bons jogadores de videogame, não sabem o que é trocar uma marcha manualmente como os deuses Ayrton Senna e Ailan Prost, era tão desgastante pilotar um carro que alguns pilotos iniciantes tinham que sair de maca após uma corrida.</p>
+    </div>
+</main>
+
+<?php
+// include do footer 
+include_once './includes/_footer.php';
