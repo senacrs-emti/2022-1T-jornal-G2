668e0f72df8449be708ece910715a6ef26b231c2
Marcelo Corrêa Padilha <108826467+lencol-oxidavel@users.noreply.github.com>
2022-11-23 23:54:08 -0300

    p
    adicionei o p

--- IEMRioMajorNews.php
+++ IEMRioMajorNews.php
@@ -5,9 +5,13 @@ include_once './includes/_header.php';
 ?>
 
 <main class="container-fluid">
-    <div class="row col-lg-4 col-12">
+    <div class="col-lg-12 col-12 pr-4 pl-4 iemriomajor">
         <h1 class="text-center">IEM Rio Major 2022</h1>
         <h2 class="text-center">Classificação da Furia</h2>
+        <img class="w-100" src="assets\IMGs\FURIA_uniforme_novo_u2ezh6c.jpg" alt="">
+        <p class="iemp">O Major de CS:GO é o campeonato mais importante do cenário profissional de Counter-Strike. Um campeonato mundial, onde os melhores jogadores do mundo todo participam. Nos anos anteriores o prize pool era um total de U$1.000.000,00 equivalente à (R$ 5,41 milhões), sofrendo um aumento no ano de 2022 para US$ 1,25 milhão (R$ 6,77 milhões).</p>
+        <p class="iemp">O Legends Stage do torneio global de CS:GO IEM Rio Major se encerrou na terça feira dia 08/11/2022, e a próxima fase, o Champions Stage, começa na quinta feira dia 10/11/2022 e termina no domingo dia 13/11/2022, com apenas 8 dos 24 times restando, sendo um deles a FURIA Esports, um time Brasileiro.</p>
+
     </div>
     
 </main>
