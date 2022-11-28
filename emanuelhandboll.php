58f8478f3284de6fad5bb865e59e36688aaefd17
Marcelo Corrêa Padilha <marcelocpadilha51@gmail.com>
2022-11-28 15:13:01 -0300

    margin

--- emanuelhandboll.php
+++ emanuelhandboll.php
@@ -6,18 +6,18 @@ include_once './includes/_header.php';
 
 <main class="conainter-fluid">  
     <div class="col-lg-12 col-12">
-        <h1 class="emanuelf">Handebol</h1>
-        <img class="w-100" id="handebol" src="https://www.rbsdirect.com.br/imagesrc/35682934.jpg?w=700&rv=2-10-05&safari" >
-        <figcaption class="emanuelh6"><strong>Autor:</strong> Porthus Junior / Agencia RBS - Fornecido: https://gauchazh.clicrbs.com.br/pioneiro/esportes/noticia/2022/05/meninas-de-bronze-brasil-vence-quenia-e-garante-medalha-no-handebol-feminino-cl3614gk9000t019iml2e845k.html</figcaption>
-        <h2 class="emanuelj">Meninas de bronze: Brasil vence Quênia e garante medalha no handebol feminino</h2>
-        <p class="emanuelp">O Brasil garantiu na manhã deste sábado (14) a sua sexta medalha nas Surdolimpíadas, superando o número da última edição, em Sansum, na Turquia, em 2017. O sexto bronze veio no handebol feminino, com vitória sobre Quênia por 24 a 15.</p>
-        <p class="emanuelp">Antes, o país já tinha subido ao pódio duas vezes com Guilherme Maia, na natação, e em duas disputas do judô, com Rômulo Crispim e Alexandre Fernandes. Além disso, já tem garantido o terceiro lugar no futebol feminino, após a desistência do Japão.</p>
-        <p class="emanuelp">Com o Ginásio Poliesportivo da UCS recebendo ótimo público, a equipe brasileira iniciou o jogo nervosa, assim como as quenianas. Os ataques demoraram a deslanchar e o placar nos primeiros cinco minutos ficou em 5 a 5. A partir daí, com uma defesa mais sólida e explorando a força e imposição da camisa 3, a paulista Fernanda Caporal, grande nome da equipe, o Brasil conseguiu abrir frente. Com isso, foi para o intervalo vencendo por 11 a 5.</p>
-        <p class="emanuelp">Nas arquibancadas, um show da parte de torcedores, integrantes de delegações de diferentes  países e alunos que foram acompanhar a decisão do handebol. Seja com gritos, luzes dos celulares, aplausos ou fazendo os sinais de comemoração em Libras, a torcida empurrou a equipe em busca da medalha inédita para a modalidade.</p>
-        <p class="emanuelp">Na volta do segundo tempo, o Brasil soube controlar a vantagem e administrar o ímpeto das quenianas, que passaram a demonstrar cansaço no decorrer do confronto. Sem a manutenção do mesmo ritmo, e com a goleira brasileira Ana fazendo ótimas intervenções, Quência não teve forças para reagir e o Brasil pôde celebrar um feito histórico, com 24 a 15 no placar.</p>
-        <p class="emanuelp">Na decisão do ouro, ainda na tarde deste sábado, a Turquia enfrenta a Dinamarca. A entrega das medalhas vai ocorrer por volta de 18h, no Ginásio da UCS.</p>
-        <img class="w-100" src="assets\IMGs\handbol.jpg" alt="">
-        <figcaption class="emanuelh6"> Fornecido: https://www.todamateria.com.br/handebol/
+        <h1 class="emanuelf pl-5 pr-5">Handebol</h1>
+        <img class="w-100 pl-5 pr-5" id="handebol" src="https://www.rbsdirect.com.br/imagesrc/35682934.jpg?w=700&rv=2-10-05&safari" >
+        <figcaption class="emanuelh6 pl-5 pr-5"><strong>Autor:</strong> Porthus Junior / Agencia RBS - Fornecido: https://gauchazh.clicrbs.com.br/pioneiro/esportes/noticia/2022/05/meninas-de-bronze-brasil-vence-quenia-e-garante-medalha-no-handebol-feminino-cl3614gk9000t019iml2e845k.html</figcaption>
+        <h2 class="emanuelj pl-5 pr-5">Meninas de bronze: Brasil vence Quênia e garante medalha no handebol feminino</h2>
+        <p class="emanuelp pl-5 pr-5">O Brasil garantiu na manhã deste sábado (14) a sua sexta medalha nas Surdolimpíadas, superando o número da última edição, em Sansum, na Turquia, em 2017. O sexto bronze veio no handebol feminino, com vitória sobre Quênia por 24 a 15.</p>
+        <p class="emanuelp pl-5 pr-5">Antes, o país já tinha subido ao pódio duas vezes com Guilherme Maia, na natação, e em duas disputas do judô, com Rômulo Crispim e Alexandre Fernandes. Além disso, já tem garantido o terceiro lugar no futebol feminino, após a desistência do Japão.</p>
+        <p class="emanuelp pl-5 pr-5">Com o Ginásio Poliesportivo da UCS recebendo ótimo público, a equipe brasileira iniciou o jogo nervosa, assim como as quenianas. Os ataques demoraram a deslanchar e o placar nos primeiros cinco minutos ficou em 5 a 5. A partir daí, com uma defesa mais sólida e explorando a força e imposição da camisa 3, a paulista Fernanda Caporal, grande nome da equipe, o Brasil conseguiu abrir frente. Com isso, foi para o intervalo vencendo por 11 a 5.</p>
+        <p class="emanuelp pl-5 pr-5">Nas arquibancadas, um show da parte de torcedores, integrantes de delegações de diferentes  países e alunos que foram acompanhar a decisão do handebol. Seja com gritos, luzes dos celulares, aplausos ou fazendo os sinais de comemoração em Libras, a torcida empurrou a equipe em busca da medalha inédita para a modalidade.</p>
+        <p class="emanuelp pl-5 pr-5">Na volta do segundo tempo, o Brasil soube controlar a vantagem e administrar o ímpeto das quenianas, que passaram a demonstrar cansaço no decorrer do confronto. Sem a manutenção do mesmo ritmo, e com a goleira brasileira Ana fazendo ótimas intervenções, Quência não teve forças para reagir e o Brasil pôde celebrar um feito histórico, com 24 a 15 no placar.</p>
+        <p class="emanuelp pl-5 pr-5">Na decisão do ouro, ainda na tarde deste sábado, a Turquia enfrenta a Dinamarca. A entrega das medalhas vai ocorrer por volta de 18h, no Ginásio da UCS.</p>
+        <img class="w-100 pl-5 pr-5" src="assets\IMGs\handbol.jpg" alt="">
+        <figcaption class="emanuelh6 pl-5 pr-5"> Fornecido: https://www.todamateria.com.br/handebol/
 
     </div>
   </main>
