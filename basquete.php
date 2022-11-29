d240493b2f3875f45c6189f83d1e345ad225349b
cauan <cauantodeschini@gmail.com>
2022-11-29 16:52:53 -0300

    att basquete esqueleto

---
+++ basquete.php
@@ -0,0 +1,17 @@
+<?php
+// include de header
+include_once './includes/_header.php';
+// parte do conteudo da pagina
+?>
+
+<main class="container-fluid">
+    <div class="row col-lg-4 col-12">
+        <h1></h1>
+        <h3></h3>
+    </div>
+    
+</main>
+
+<?php
+// include do footer 
+include_once './includes/_footer.php';
