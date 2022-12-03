710f2ac451f2f57eeb3753a47987b01f44d24ce9
João Pedro Machado da Silva <108826553+machadodelta@users.noreply.github.com>
2022-12-02 22:37:25 -0300

    seja o que Deus quiser
    não sei como ficaram as alterações feitas aqui, meu Xampp da dando pau.

---
+++ cronicaf1.php
@@ -0,0 +1,33 @@
+<?php
+// include de header
+include_once './includes/_header.php';
+// parte do conteudo da pagina
+?>
+
+<main class="container-fluid">
+    <div class="row col-lg-4 col-12">
+        <h1 class=" pl-10" id="h1f1">O DIA MAIS TRISTE PARA O BRASIL</h1>
+
+    </div>
+    <p>Ultimamente o grid vem tendo os seus favoritos, corredores como Max Verstappen ou Charles Leclercq.
+       É uma pena todo esse favoritismo imposto em cima dos mesmos corredores. A nova geração da Fórmula 1 deixa
+       a desejar com seus carros praticamente automáticos, os melhores pilotos são apenas bons jogadores de videogame, não sabem o que é trocar uma marcha manualmente, como os deuses Ayrton Senna e Ailan Prost.
+       Era tão desgastante pilotar um carro que alguns pilotos iniciantes tinham que sair de maca após uma corrida.  </p>
+
+       <img src="assets\IMGs\compara.webp" alt="" srcset="">
+
+       
+       <p>Não estou dizendo que as corridas de hoje em dia não são interessantes, mas a tecnologia não é exatamente algo que melhore o entretenimento, mas sim a segurança dos pilotos, que também é importante, porém eles estão ficando mal-acostumados e está se perdendo a tradição no esporte. </p>
+
+       <p>Usemos como exemplo o tão conhecido piloto Lewis Hamilton, que teve seu ápice em 2021 e agora em 2022 não está nem perto do top 5. Não é de ontem que sabemos que corridas de Fórmula 1 são compradas de uns anos para cá, como por exemplo na corrida em que um piloto parou no meio da linha de chegada para não ganhar a prova, sendo obviamente um comando de sua equipe para que ele perdesse propositalmente. A ganância dos milionários infectou até mesmo os esportes, é uma pena ver isso acontecendo aos poucos. </p>
+
+       <p>Essa influência é tão grande que o GP da Rússia foi cancelado devido à guerra na Ucrânia e continuará assim por bom tempo, já que a Rússia vem sendo banida de todos os tipos de esportes. Em resumo, a meu ver, tudo vem se modificando para tornar as coisas mais simples, porém chatas. </p>
+
+       <p>Mas não pensem que eu estou renunciando a toda a segurança dos pilotos para querer entretenimento a mais para nós telespectadores, de forma alguma, apenas acho que a corrupção chegou até mesmo aos esportes. Junto disso, uma forma de manipular as corridas e os corredores. Uma pena para o esporte mesmo. Aguardo o dia em que verei alguém correr como o Ayrton Senna novamente. </p>
+
+</main>
+
+
+<?php
+// include do footer 
+include_once './includes/_footer.php';
--- includes/_header.php
+++ includes/_header.php
@@ -39,7 +39,7 @@
 
       <a class="dropdown-item" href="preconvoca.php">Pré-Convocação</a>
 
-      <a class="dropdown-item" href="#">Something else here</a>
+      <a class="dropdown-item" href="noticiaf1.php">GP do México F1</a>
       <a class="dropdown-item" href="Convocados.php">Convocados para o Copa do Mundo</a>
 
     </div>
@@ -48,7 +48,7 @@
     <a class="nav-link dropdown-toggle" data-toggle="dropdown" href="#" role="button" aria-expanded="false">Crônicas</a>
     <div class="dropdown-menu">
       <a class="dropdown-item" href="#">Action</a>
-      <a class="dropdown-item" href="#">Another action</a>
+      <a class="dropdown-item" href="cronicaf1.php">Últimos momentos de Senna</a>
       <a class="dropdown-item" href="copa.php">Copa do Mundo</a>
     </div>
   </li>
